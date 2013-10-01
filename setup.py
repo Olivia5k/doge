ed08315dc2813f3773dd0c8a5749f859e475426b
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-10-01 15:35:45 +0200

    wow python packaging

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name='doge',
-    version='0.1-dev',
+    version='0.3',
     url='https://github.com/thiderman/doge',
     author='Lowe Thiderman',
     author_email='lowe.thiderman@gmail.com',
     description=('wow very doge'),
     license='MIT',
-    entry_points={
-        'console_scripts': [
-            'doge = doge.doge:main'
-        ]
-    },
+    packages=['doge'],
+    package_data={'doge': ['*/*.txt']},
+    scripts=[
+        'bin/doge'
+    ],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: Unix',
     ],
 )
