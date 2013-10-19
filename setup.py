6ff2d5a348f32584c9ec81095f1cee80e9187a48
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-10-19 17:35:27 +0200

    wow bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='doge',
-    version='0.6.0',
+    version='0.6.1',
     url='https://github.com/thiderman/doge',
     author='Lowe Thiderman',
     author_email='lowe.thiderman@gmail.com',
