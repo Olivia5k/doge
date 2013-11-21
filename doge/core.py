00037e40dc7f21d18ca0aff2ebbde704aad619ca
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-11-21 18:11:04 +0100

    wow so split doge
    such split to core and wow
    many big change
    so bump version

--- doge/core.py
+++ doge/core.py
@@ -13,48 +13,13 @@ import subprocess as sp
 from os.path import dirname, join
 from os import environ
 
+from doge import wow
+
 ROOT = dirname(__file__)
 
 
 class Doge(object):
     default_doge = join(ROOT, 'static/doge.txt')
-
-    # wow pls extend this if you see it
-    known_processes = (
-        # wow shells and terminals and etc
-        'urxvt', 'tmux', 'fish', 'ssh', 'mutt', 'screen', 'mosh-client',
-        'Terminal',
-
-        # pretty browsers
-        'chromium', 'luakit', 'uzbl-core', 'firefox', 'jumanji', 'chrome',
-        'thunderbird', 'Safari', 'Google Chrome',
-
-        # many wms (borrow from djmelik/archey)
-        'awesome', 'beryl', 'blackbox', 'bspwm', 'compiz', 'dwm',
-        'enlightenment', 'herbstluftwm', 'fluxbox', 'fvwm', 'i3', 'icewm',
-        'kwin', 'metacity', 'musca', 'openbox', 'pekwm', 'ratpoison',
-        'scrotwm', 'wmaker', 'wmfs', 'wmii', 'xfwm4', 'xmonad',
-        'gnome-shell', 'devilspie2',
-
-        # such services and daemons
-        'mpd', 'nginx', 'dzen2', 'systemd', 'lighttpd', 'prosody',
-        'mongod', 'postgres', 'mysqld', 'redis-server', 'php-fpm',
-        'sendmail', 'irssi', 'screen', 'neo4j', 'httpd', 'Flux', 'Dropbox',
-        'VirtualBox', 'VirtualBoxVM', 'tmux', 'xflux', 'weechat', 'xen',
-        'apache', 'sshd',
-
-        # programming languages
-        'ruby', 'python', 'java', 'php', 'node', 'erlang', 'perl',
-
-        # so editors
-        'emacs', 'vim', 'nano', 'gedit', 'sublime_text', 'subl', 'other',
-
-        # very talk
-        'HipChat', 'Mail',
-
-        # shells
-        'sh', 'bash', 'zsh'
-    )
 
     def __init__(self, tty, doge_path=default_doge):
         self.tty = tty
@@ -141,7 +106,7 @@ class Doge(object):
             self.real_data.append(random.choice(files))
 
         for proc in self.get_processes():
-            if proc in self.known_processes:
+            if proc in wow.KNOWN_PROCESSES:
                 self.real_data.append(proc)
                 break
 
@@ -176,22 +141,6 @@ class Doge(object):
 
 
 class DogeMessage(object):
-    prefixes = [
-        'wow', 'such', 'very', 'so much', 'many', 'lol', 'beautiful',
-        'all the', 'the', 'most', 'very much', 'pretty', 'so',
-    ]
-
-    words = [
-        'computer', 'hax0r', 'code', 'data', 'internet', 'server',
-        'hacker', 'terminal', 'doge', 'shibe', 'program', 'free software',
-        'web scale', 'monads', 'git', 'daemon', 'loop', 'pretty', 'uptime',
-        'thread safe', 'posix',
-    ]
-
-    suffixes = [
-        'wow', 'lol', 'hax', 'plz', 'lvl=100'
-    ]
-
     def __init__(self, tty, occupied, word=None):
         self.tty = tty
         self.occupied = occupied
@@ -229,12 +178,12 @@ class DogeMessage(object):
             msg = self.word
         else:
             if not self.word:
-                self.word = random.choice(self.words)
+                self.word = random.choice(wow.WORDS)
 
-            msg = '{0} {1}'.format(random.choice(self.prefixes), self.word)
+            msg = '{0} {1}'.format(random.choice(wow.PREFIXES), self.word)
 
             if random.choice(range(15)) == 0:
-                msg += ' {0}'.format(random.choice(self.suffixes))
+                msg += ' {0}'.format(random.choice(wow.SUFFIXES))
 
         self.orig_message = msg
         self.message = msg
@@ -246,27 +195,11 @@ class DogeMessage(object):
 
         # very ansi code wow
         self.message = '[1m[38;5;{0}m{1}[39m[0m'.format(
-            random.choice(self.tty.colors), self.message
+            random.choice(wow.COLORS), self.message
         )
 
 
 class TTYHandler(object):
-    # wow remove darkest unread color
-    colors = (
-        23, 24, 25, 26, 27, 29, 30, 31, 32, 33, 35, 36, 37, 38, 39, 41, 42, 43,
-        44, 45, 47, 48, 49, 50, 51, 58, 59, 63, 64, 65, 66, 67, 68, 69, 70, 71,
-        72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 94,
-        95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
-        110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123,
-        130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143,
-        144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157,
-        158, 159, 162, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176,
-        177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190,
-        191, 192, 193, 194, 195, 197, 202, 203, 204, 205, 206, 207, 208, 209,
-        210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
-        224, 225, 226, 227, 228
-    )
-
     def setup(self):
         self.height, self.width = self.get_tty_size()
         self.is_tty = sys.stdout.isatty()
---
+++ doge/wow.py
@@ -0,0 +1,65 @@
+# wow pls extend this if you see it
+KNOWN_PROCESSES = (
+    # wow shells and terminals and etc
+    'urxvt', 'tmux', 'fish', 'ssh', 'mutt', 'screen', 'mosh-client',
+    'Terminal', 'sh', 'bash', 'zsh'
+
+    # pretty browsers
+    'chromium', 'luakit', 'uzbl-core', 'firefox', 'jumanji', 'chrome',
+    'thunderbird', 'Safari', 'Google Chrome',
+
+    # many wms
+    'awesome', 'beryl', 'blackbox', 'bspwm', 'compiz', 'dwm',
+    'enlightenment', 'herbstluftwm', 'fluxbox', 'fvwm', 'i3', 'icewm',
+    'kwin', 'metacity', 'musca', 'openbox', 'pekwm', 'ratpoison',
+    'scrotwm', 'wmaker', 'wmfs', 'wmii', 'xfwm4', 'xmonad',
+    'gnome-shell', 'devilspie2',
+
+    # such services and daemons
+    'mpd', 'nginx', 'dzen2', 'systemd', 'lighttpd', 'prosody',
+    'mongod', 'postgres', 'mysqld', 'redis-server', 'php-fpm',
+    'sendmail', 'irssi', 'screen', 'neo4j', 'httpd', 'Flux', 'Dropbox',
+    'VirtualBox', 'VirtualBoxVM', 'tmux', 'xflux', 'weechat', 'xen',
+    'apache', 'sshd',
+
+    # programming languages
+    'ruby', 'python', 'java', 'php', 'node', 'erlang', 'perl',
+
+    # so editors
+    'emacs', 'vim', 'nano', 'gedit', 'sublime_text', 'subl',
+
+    # very talk
+    'HipChat', 'Mail',
+)
+
+PREFIXES = [
+    'wow', 'such', 'very', 'so much', 'many', 'lol', 'beautiful',
+    'all the', 'the', 'most', 'very much', 'pretty', 'so',
+]
+
+WORDS = [
+    'computer', 'hax0r', 'code', 'data', 'internet', 'server',
+    'hacker', 'terminal', 'doge', 'shibe', 'program', 'free software',
+    'web scale', 'monads', 'git', 'daemon', 'loop', 'pretty', 'uptime',
+    'thread safe', 'posix',
+]
+
+SUFFIXES = [
+    'wow', 'lol', 'hax', 'plz', 'lvl=100'
+]
+
+# wow remove darkest unread color
+COLORS = (
+    23, 24, 25, 26, 27, 29, 30, 31, 32, 33, 35, 36, 37, 38, 39, 41, 42, 43,
+    44, 45, 47, 48, 49, 50, 51, 58, 59, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+    72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 94,
+    95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+    110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123,
+    130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143,
+    144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157,
+    158, 159, 162, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176,
+    177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190,
+    191, 192, 193, 194, 195, 197, 202, 203, 204, 205, 206, 207, 208, 209,
+    210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
+    224, 225, 226, 227, 228
+)
