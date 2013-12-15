38cbfa81f294ee57708b3176d1028fe3ba2c5f92
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-12-15 23:56:01 +0100

    wow bamp version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='doge',
-    version='3.0.0',
+    version='3.1.0',
     url='https://github.com/thiderman/doge',
     author='Lowe Thiderman',
     author_email='lowe.thiderman@gmail.com',
