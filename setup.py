f46a2653b1585d4b2e5169f719070857327d1c87
Lowe Thiderman <lowe.thiderman@gmail.com>
2013-12-31 14:27:08 +0100

    wow bamp version
    </doge>
    
    Fixes #41. The issue itself was fixed in 63bcb9ba as a an unrelated post-fix to
    some Windows tuning. Encodings will one day be the death of me.
    
    Anyway, now Shibe can enter the new year without any known issues. such code,
    many wow. <3 Happy new year, everyone.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='doge',
-    version='3.3.0',
+    version='3.4.0',
     url='https://github.com/thiderman/doge',
     author='Lowe Thiderman',
     author_email='lowe.thiderman@gmail.com',
